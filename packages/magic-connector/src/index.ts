23dfa8dc94a3aa9a4889e5989fc9f7b396cf8ce0
Mana <134029797+whatmanathinks@users.noreply.github.com>
2023-08-15 15:20:25 +0530

    feat: added exports for MagicConnectors (base, social and email)

--- packages/magic-connector/src/index.ts
+++ packages/magic-connector/src/index.ts
@@ -1,3 +1,7 @@
+export {MagicAuthConnector} from "./connectors/auth";
+export {MagicConnector} from "./connectors/base";
+export {MagicSocialConnector} from "./connectors/social";
+
 export {emailMagicWallet} from "./wallets/email";
 export {phoneMagicWallet} from "./wallets/phone";
 export {socialMagicWallet} from "./wallets/social";
